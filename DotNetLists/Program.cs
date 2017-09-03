a1988f331989808ad04ab0827045ea10fd4ff924
ikostan <superegorius@gmail.com>
2017-09-03 01:19:13 -0700

    dictionarySample

--- DotNetLists/Program.cs
+++ DotNetLists/Program.cs
@@ -12,13 +12,42 @@ namespace DotNetLists
             {
 
             listSample();
+            dictionarySample();
 
             Console.WriteLine("\nPlease press any key...");
             Console.ReadLine(); //Wait for input before closing the console
 
             }
 
+
+        private static void dictionarySample() {
+
+            Console.WriteLine("Dictionaries for key value pairs:");
+
+            Dictionary<String, String> configs = new Dictionary<String, String>();
+
+            configs.Add("resolution", "1920x1080");
+            configs.Add("title", "My website");
+            configs.Add("name", "My name is John");
+
+            foreach (var config in configs)
+                {
+
+                Console.WriteLine(config);
+                }
+
+            //Show item by key
+            Console.WriteLine(configs["name"]);
+
+
+            }
+
+
+
         private static void listSample() {
+
+            Console.WriteLine("Lists for indexed values:");
+
 
             //Lists
             List<String> customers = new List<String>();
