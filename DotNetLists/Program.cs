8b16e565510256b331169bfd1820c621a73ef2ea
superadmin <superadmin@DESKTOP-V7IHS80>
2017-09-03 00:54:30 -0700

    New file

--- DotNetLists/Program.cs
+++ DotNetLists/Program.cs
@@ -10,6 +10,20 @@ namespace DotNetLists
         {
         static void Main(string[] args)
             {
+
+            //Lists
+            List<String> customers = new List<String>();
+            customers.Add("Kim");
+            customers.Add("Ted");
+            customers.Add("John");
+
+            foreach (var customer in customers) {
+                Console.WriteLine(customer);
+            }
+
+            Console.WriteLine("\nPlease press any key...");
+            Console.ReadLine(); //Wait for input before closing the console
+
             }
         }
     }
