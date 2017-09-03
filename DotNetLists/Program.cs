5d3c0dca066b7f43a85b2391c05a7c7c8e1f3d11
ikostan <superegorius@gmail.com>
2017-09-03 01:12:05 -0700

    New method added

--- DotNetLists/Program.cs
+++ DotNetLists/Program.cs
@@ -11,19 +11,32 @@ namespace DotNetLists
         static void Main(string[] args)
             {
 
+            listSample();
+
+            Console.WriteLine("\nPlease press any key...");
+            Console.ReadLine(); //Wait for input before closing the console
+
+            }
+
+        private static void listSample() {
+
             //Lists
             List<String> customers = new List<String>();
             customers.Add("Kim");
             customers.Add("Ted");
             customers.Add("John");
 
-            foreach (var customer in customers) {
-                Console.WriteLine(customer);
+            foreach (var customer in customers)
+                {
+
+                    Console.WriteLine(customer);
+                }
+
+            //Show specific customer
+            Console.WriteLine(customers[1]);
+
             }
 
-            Console.WriteLine("\nPlease press any key...");
-            Console.ReadLine(); //Wait for input before closing the console
 
-            }
         }
     }
