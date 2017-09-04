b9b97f0c88727ae028c786cb490d750d3bcbe32e
ikostan <superegorius@gmail.com>
2017-09-04 15:14:41 -0700

    static void FormatNumericalData()

--- DotNetLists/Program.cs
+++ DotNetLists/Program.cs
@@ -39,6 +39,21 @@ namespace DotNetLists
 
             }
 
+        static void FormatNumericalData()
+            {
+            Console.WriteLine("\nThe value 99999 in various formats:\n");
+            Console.WriteLine("c format: {0:c}", 99999);
+            Console.WriteLine("d9 format: {0:d9}", 99999);
+            Console.WriteLine("f3 format: {0:f3}", 99999);
+            Console.WriteLine("n format: {0:n}", 99999);
+            // Notice that upper- or lowercasing for hex
+            // determines if letters are upper- or lowercase.
+            Console.WriteLine("E format: {0:E}", 99999);
+            Console.WriteLine("e format: {0:e}", 99999);
+            Console.WriteLine("X format: {0:X}", 99999);
+            Console.WriteLine("x format: {0:x}", 99999);
+            }
+
         static void ShowEnvironmentDetails()
             {
             // Print out the drives on this machine,
@@ -170,6 +185,7 @@ namespace DotNetLists
                 setStack();
                 setQueue();
                 setHashSet();
+                FormatNumericalData();
 
                 Console.WriteLine("\nPress ENTER key to continue...");
                 Console.ReadLine(); // Wait for Enter key to be pressed.
