fd670225398d5a1914952b5ace00820eefd85654
ikostan <superegorius@gmail.com>
2017-09-03 23:19:49 -0700

    ShowEnvironmentDetails

--- DotNetLists/Program.cs
+++ DotNetLists/Program.cs
@@ -33,9 +33,23 @@ namespace DotNetLists
 
             mainProgram();
             startMultiThreads();
+            ShowEnvironmentDetails();
 
             MessageBox.Show("All done!");
 
+            }
+
+        static void ShowEnvironmentDetails()
+            {
+            // Print out the drives on this machine,
+            // and other interesting details.
+            foreach (string drive in Environment.GetLogicalDrives())
+                Console.WriteLine("Drive: {0}", drive);
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}",
+            Environment.ProcessorCount);
+            Console.WriteLine(".NET Version: {0}",
+            Environment.Version);
             }
 
         private static void setStack() {
